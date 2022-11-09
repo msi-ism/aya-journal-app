0467f067fbd15bd808938f8e31e582d28839336f
msi-ism <msism720@gmail.com>
2022-11-08 21:05:57 -0500

    homepage skeleton done

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -3,7 +3,17 @@
 const PostCard = () => {
     return (
         <div className="post-card">
-            <h2>Postcard 1</h2>
+            <p>Post Author</p>
+            <h2>Post Title</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat non tortor non venenatis. Nulla eget erat pellentesque, ultrices diam.
+            </p>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat non tortor non venenatis. Nulla eget erat pellentesque, ultrices diam.
+            </p>
+            <div className="lower-card">
+                <p>Claps</p>
+                <p>Comments</p>
+            </div>
         </div>
     );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,41 @@
 import PostCard from "../components/PostCard";
+import { useEffect, useState } from "react";
+
+const verbs = ['be', 'see', 'know', 'grow', 'discover', 'love', 'heal']
+
 
 const HomePage = () => {
-        return (
-            <div className="main-container">
-                <div className="header">
-                    <h1>Welcome Home!</h1>
-                </div>
-                <div className="home-cardcontainer">
-                    <PostCard />
-                    <PostCard />
-                    <PostCard />
-                    <PostCard />
-                    <PostCard />
-                </div>
+    const [verb, setVerb] = useState('be')
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            for (let i = 0; i < verbs.length; i++) {
+                let randomVerb = verbs[Math.floor(Math.random() * verbs.length)]
+                setVerb((verb) => randomVerb)
+        }
+            
+
+        }, 1250)
+        // ^ Above all, when using setInterval, it is imperative that you clear the scheduled interval once the component unmounts.
+        return () => clearInterval(interval)
+
+    }, [])
+
+    return (
+        <div className="main-container">
+            <div className="header">
+                <h1>Welcome to the As You Are Community!</h1>
+                <h2>A place where you can<span className='magic-word'> {verb}</span> yourself as you are.</h2>
             </div>
-        );
-    }
+            <div className="home-cardcontainer">
+                <PostCard />
+                <PostCard />
+                <PostCard />
+                <PostCard />
+                <PostCard />
+            </div>
+        </div>
+    );
+}
 
 export default HomePage
