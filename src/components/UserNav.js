3e5f27c6f73fd2fc82733fec4e6c04b41ac81ead
msi-ism <msism720@gmail.com>
2022-11-09 14:30:08 -0500

    user sidebar nav skeleton done

--- src/components/UserNav.js
+++ src/components/UserNav.js
@@ -1,11 +1,18 @@
+import profilePic from './account.png'
+
 
 const UserNav = () => {
     return (
         <div className='user-nav'>
-            <div className="user-nav-item user-block">User Block</div>
-            <div className='user-nav-item user-link'>Link 1</div>
-            <div className='user-nav-item user-link'>Link 2</div>
-            <div className='user-nav-item user-link'>Link 3</div>
+            <div className="user-nav-item user-block">
+                <img className='profile-pic' src={profilePic}/>
+                <h4>User Block</h4>
+            </div>
+            <div className='user-links'>
+                <div className='user-nav-item user-link'>Link 1</div>
+                <div className='user-nav-item user-link'>Link 2</div>
+                <div className='user-nav-item user-link'>Link 3</div>
+            </div>
             <div className='acct-block'>
                 <div className="user-nav-item acct-link">Account Link 1</div>
                 <div className="user-nav-item acct-link">Account Link 2</div>
