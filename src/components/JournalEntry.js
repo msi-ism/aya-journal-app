c36fbdbdf7b3bbf8d4a475c4d1f74a1e161685e2
msi-ism <msism720@gmail.com>
2022-11-11 18:30:35 -0500

    added btn text switching on state switch

--- src/components/JournalEntry.js
+++ src/components/JournalEntry.js
@@ -1,37 +1,42 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 
 const closeWindow = () => {
     let journalCanvas = document.querySelector('.journal-container')
     journalCanvas.style.visibility = 'hidden'
 }
+
 let privatePost = true
 
-const switchPrivacy = () => {
-    let privacyBtn = document.querySelector('.privacy-btn')
-    if (privatePost) {
-        privacyBtn.textContent = 'Public'
-        privatePost = false
-    } else {
-        privacyBtn.textContent = 'Private'
-        privatePost = true
+const JournalEntry = () => {
+    const [mode, setMode] = useState('Private')
+
+    
+    const switchPrivacy = (mode) => {
+        if (privatePost) {
+            setMode('Publish')
+            privatePost = false
+            console.log('public')
+        } else {
+            setMode('Save')
+            privatePost = true
+            console.log('is private again')
+        }
     }
 
-}
+    
 
-
-const JournalEntry = () => {
     useEffect(() => {
         let privacyBtn = document.querySelector('.privacy-btn')
         privacyBtn.textContent = 'Private'
         console.log(privacyBtn)
     }, [])
-    
+
     return (
         <div className='journal-container'>
             <div className='journal-canvas'>
                 <div className='upper-canvas'>
                     <div className='canvas-title'>
-                    <h1>Question Prompt or Title</h1>
+                        <h1>Question Prompt or Title</h1>
                     </div>
                     <div className='canvas-exit' onClick={closeWindow}>
                         X
@@ -41,12 +46,12 @@ const JournalEntry = () => {
                     <textarea placeholder="What's on your mind?"></textarea>
                 </div>
                 <div className='lower-canvas'>
-                    <button className='privacy-btn' onClick={switchPrivacy}>Public</button>
-                    <input type='submit' value='Publish'></input>
+                    <button className='privacy-btn' onClick={switchPrivacy}>{privatePost ? 'Private' : 'Public'}</button>
+                    <button className='privacy-btn' type='submit'>{mode}</button>
                 </div>
 
             </div>
-            
+
         </div>
     );
 }
