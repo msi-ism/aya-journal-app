9d3b36e8b0c038e723d403ed42b4d00bdee13e86
msi-ism <msism720@gmail.com>
2022-11-15 08:22:04 -0500

    fixed signup login btn size & fixed feed page margins

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -80,7 +80,7 @@ export default class SignUpForm extends Component {
                 <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
                 <label>Confirm</label>
                 <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
-                <button type="submit" disabled={disable}>SIGN UP</button>
+                <button className='signup-btn' type="submit" disabled={disable}>SIGN UP</button>
               </form>
             </div>
             <p className="error-message">&nbsp;{this.state.error}</p>
